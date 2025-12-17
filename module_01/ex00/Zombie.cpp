d436fea70c829652b3991a0db501bd134f6ed962
c4v3d <theoimmi@pm.me>
2025-12-17 14:39:15 +0100

    finished

--- module_01/ex00/Zombie.cpp
+++ module_01/ex00/Zombie.cpp
@@ -1,20 +1,16 @@
 #include "Zombie.hpp"
 #include <iostream>
 
-Zombie::Zombie(void): _name("Robert") {
-	this->announce();
+Zombie::Zombie(void): _name("Daryl") {
 }
 
 Zombie::Zombie(std::string name): _name(name) {
-	this->announce();
 }
 
 Zombie::Zombie(const Zombie &other): _name(other._name) {
-	this->announce();
 }
 
 Zombie &Zombie::operator=(const Zombie &other) {
-	this->announce();
 	if (this != &other) {
 		this->_name = other._name;
 	}
@@ -22,6 +18,7 @@ Zombie &Zombie::operator=(const Zombie &other) {
 }
 
 Zombie::~Zombie(void) {
+	std::cout << this->_name << ": has been killed..." << std::endl;
 }
 
 void	Zombie::announce() {
--- module_01/ex00/main.cpp
+++ module_01/ex00/main.cpp
@@ -2,9 +2,27 @@
 #include <iostream>
 
 Zombie* newZombie(std::string name);
+void	randomChump(std::string name);
 
 int	main(void) {
-	Zombie *z = newZombie("test");
-	(void)z;
+
+	std::cout << "Creating Zombie from default constructor." << std::endl;
+	Zombie	def;
+	def.announce();
+
+	std::cout << "Creating Zombie with parameter." << std::endl;
+	Zombie	par("Carl");
+	par.announce();
+
+	std::cout << "Creating Zombie from newZombie function." << std::endl;
+	Zombie *rick = newZombie("Rick");
+	if (!rick)
+		return (1);
+	rick->announce();
+	delete rick;
+
+	std::cout << "Creating Zombie from randomChump." << std::endl;
+	randomChump("Michonne");
+
 	return (0);
 }
