3a41fb05b525584358e62cdeadcc136180bc9d5f
c4v3d <theoimmi@pm.me>
2025-12-17 14:41:21 +0100

    removed unused methods

--- module_01/ex00/Zombie.cpp
+++ module_01/ex00/Zombie.cpp
@@ -7,16 +7,6 @@ Zombie::Zombie(void): _name("Daryl") {
 Zombie::Zombie(std::string name): _name(name) {
 }
 
-Zombie::Zombie(const Zombie &other): _name(other._name) {
-}
-
-Zombie &Zombie::operator=(const Zombie &other) {
-	if (this != &other) {
-		this->_name = other._name;
-	}
-	return (*this);
-}
-
 Zombie::~Zombie(void) {
 	std::cout << this->_name << ": has been killed..." << std::endl;
 }
