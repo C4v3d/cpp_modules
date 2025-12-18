9fcf275b739f696ad932c4cfe0a98319e0d5127d
c4v3d <theoimmi@pm.me>
2025-12-19 00:01:26 +0100

    zombiiiie

---
+++ module_01/ex01/Zombie.hpp
@@ -0,0 +1,20 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+
+class Zombie {
+public :
+	Zombie	(void);
+	Zombie	(std::string name);
+	~Zombie	(void);
+
+	void	announce(void);
+	void	setName(const std::string name);
+private:
+	std::string	_name;
+
+};
+
+#endif
+
