28c0bf32d4832f4072dd1e74f4401b807db99cb4
c4v3d <theoimmi@pm.me>
2026-01-01 20:41:28 +0100

    ClapTrap

--- module_03/ex00/main.cpp
+++ module_03/ex00/main.cpp
@@ -3,13 +3,42 @@
 
 int	main(void) {
 	ClapTrap	a("Bob");
-
 	std::cout << a << std::endl;
 
+	std::cout << "Bob attack 5 times" << std::endl;
 	for (int i = 0; i < 5; i++) {
 		a.attack("John");
 	}
+	std::cout << a << std::endl;
+
+	std::cout << "Bob takes 5 damage" << std::endl;
+	a.takeDamage(5);
+	std::cout << a << std::endl;
+
+	std::cout << "Bob repairs itself 5 times for 1 HP" << std::endl;
+	for (int i = 0; i < 5; i++)
+		a.beRepaired(1);
+	std::cout << a << std::endl;
+
+	std::cout << "Bob attack but has no energy left" << std::endl;
+	a.attack("John");
+	std::cout << a << std::endl;
+
+	std::cout << "Bob repairs but has no energy left" << std::endl;
+	a.beRepaired(100);
+	std::cout << a << std::endl;
+
+	std::cout << "Bob takes 25 damage" << std::endl;
+	a.takeDamage(25);
+	std::cout << a << std::endl;
+
+	std::cout << "Bob repairs but is dead" << std::endl;
+	a.beRepaired(50);
+
+	std::cout << "Bob attack but is dead" << std::endl;
+	a.attack("John");
 
 	std::cout << a << std::endl;
+
 	return (0);
 }
