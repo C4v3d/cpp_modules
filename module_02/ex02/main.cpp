510dce8c090c21f304413555085fedb49636ba4d
c4v3d <theoimmi@pm.me>
2026-01-01 16:29:26 +0100

    example added in main

--- module_02/ex02/main.cpp
+++ module_02/ex02/main.cpp
@@ -2,11 +2,22 @@
 #include <iostream>
 
 int	main(void) {
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
 
-	Fixed	a(2.2f);
-	Fixed	b(1);
+	a = Fixed( 1234.4321f );
 
-	if (a < b)
-		std::cout << "A is smaller than b" << std::endl;
-	return (0);
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return 0;
 }
