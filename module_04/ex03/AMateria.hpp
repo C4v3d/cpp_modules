2bea9bcb77b69924a6b35184c164ec41d846cdb6
c4v3d <theoimmi@pm.me>
2026-01-05 20:06:12 +0100

    mhh

---
+++ module_04/ex03/AMateria.hpp
@@ -0,0 +1,20 @@
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+#include <string>
+
+class AMateria {
+	protected:
+		std::string	type;
+	public:
+		AMateria(std::string const& type);
+		AMateria(const AMateria& other);
+		AMateria&	operator=(const AMateria& other);
+		~AMateria();
+
+		std::string const& getType() const; //Returns the materia type
+		
+		virtual	AMateria* clone() const = 0;
+		virtual	void use(ICharacter& target);
+};
+#endif
