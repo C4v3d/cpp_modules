3e7f1cd0bc4df5285b3138c5842a3a20d2349de5
c4v3d <theoimmi@pm.me>
2026-01-08 19:59:54 +0100

    Fixed fixed point

--- module_02/ex02/Fixed.hpp
+++ module_02/ex02/Fixed.hpp
@@ -34,10 +34,10 @@ class Fixed {
 		Fixed&	operator--(void);
 		Fixed	operator--(int);
 
-		Fixed&	min(Fixed& a, Fixed& b);
-		const Fixed&	min(const Fixed& a, const Fixed& b);
-		Fixed&	max(Fixed& a, Fixed& b);
-		const Fixed&	max(const Fixed& a, const Fixed& b);
+		static Fixed&	min(Fixed& a, Fixed& b);
+		static const Fixed&	min(const Fixed& a, const Fixed& b);
+		static Fixed&	max(Fixed& a, Fixed& b);
+		static const Fixed&	max(const Fixed& a, const Fixed& b);
 
 	private:
 		int			_fixedPoint;
