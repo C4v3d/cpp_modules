1c91c73707ab16ea0e9ebf3b4b68f1bb4d106443
c4v3d <theoimmi@pm.me>
2026-01-08 18:08:05 +0100

    corrections

--- module_02/ex02/Fixed.cpp
+++ module_02/ex02/Fixed.cpp
@@ -119,3 +119,27 @@ Fixed Fixed::operator--(int) {
 	this->_fixedPoint--;
 	return (tmp);
 }
+
+Fixed&	Fixed::min(Fixed& a, Fixed& b) {
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+const Fixed&	Fixed::min(const Fixed& a, const Fixed& b) {
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+Fixed&	Fixed::max(Fixed& a, Fixed& b) {
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b) {
+	if (a > b)
+		return (a);
+	return (b);
+}
