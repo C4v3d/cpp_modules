cc8397f64794c50603c666b7bf3ddc0054be8293
C4v3d <theoimmi@pm.me>
2026-02-25 10:42:19 +0100

    closer

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -3,30 +3,36 @@
 
 #include <stack>
 
-/* Use `c` member object to access underlying container */
+/* Use `c` member object to access underlying container
+ * The stack "underlying container" is deque by default,
+ * but can be a vector or a list as well. */
 
-template <typename T>
+template < typename T, typename container = std::deque<T> >
 class MutantStack : public std::stack<T> {
 public:
-  MutantStack();
-  MutantStack(MutantStack const & other);
-  MutantStack&  operator=(MutantStack const & other);
-  ~MutantStack();
+	MutantStack();
+	MutantStack(MutantStack const & other);
+	~MutantStack();
 
-  MutantStack<T>::container_type::iterator begin();
-  MutantStack<T>::container_type::iterator end();
+	typedef typename container::iterator	it;
+
+	it	begin();
+	it	end();
 };
 
-template <typename T>
+template < typename T, typename Container = std::deque<T> >
 MutantStack::MutantStack(): std::stack<T>::stack() {}
-template <typename T>
+
+template < typename T, typename Container = std::deque<T> >
 MutantStack::MutantStack(MutantStack const & other): std::stack<T>::stack(other) {}
-template <typename T>
+
+template < typename T, typename Container = std::deque<T> >
 MutantStack::~MutantStack() {}
 
-template <typename T>
-typename MutantStack<T>::container_type::iterator MutantStack<T>::begin() { return (this->c.being()); }
-template <typename T>
-typename MutantStack<T>::container_type::iterator MutantStack<T>::end() { return (this->c.end()); }
+template < typename T, typename Container = std::deque<T> >
+it	MutantStack::begin() { return (this->c.being()); }
+
+template < typename T, typename Container = std::deque<T> >
+it	MutantStack<T>::end() { return (this->c.end()); }
 
 #endif
--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -1,7 +1,7 @@
 #include "MutantStack.hpp"
 
 int main(void) {
-  MutantStack<int> mutant;
-  mutant.push(3);
-  return (0);
+ 	MutantStack<int> mutant;
+ 	mutant.push(3);
+ 	return (0);
 }
