791f06c59d165668a23dba9afbd862099338772a
C4v3d <theoimmi@pm.me>
2026-02-25 11:20:36 +0100

    mhhhh

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -7,32 +7,37 @@
  * The stack "underlying container" is deque by default,
  * but can be a vector or a list as well. */
 
-template < typename T, typename container = std::deque<T> >
+template < typename T >
 class MutantStack : public std::stack<T> {
 public:
 	MutantStack();
 	MutantStack(MutantStack const & other);
+	MutantStack&	operator=(MutantStack const & other);
 	~MutantStack();
 
-	typedef typename container::iterator	it;
+	typedef typename MutantStack<T>::container_type::iterator	it;
 
 	it	begin();
 	it	end();
 };
 
-template < typename T, typename Container = std::deque<T> >
-MutantStack::MutantStack(): std::stack<T>::stack() {}
 
-template < typename T, typename Container = std::deque<T> >
-MutantStack::MutantStack(MutantStack const & other): std::stack<T>::stack(other) {}
+template < typename T >
+MutantStack<T>::MutantStack(): std::stack<T>::stack() {}
 
-template < typename T, typename Container = std::deque<T> >
-MutantStack::~MutantStack() {}
+template < typename T >
+MutantStack<T>::MutantStack(MutantStack const & other): std::stack<T>::stack(other) {}
 
-template < typename T, typename Container = std::deque<T> >
-it	MutantStack::begin() { return (this->c.being()); }
+template < typename T >
+MutantStack<T>&	MutantStack<T>::operator=(MutantStack const & other) { return *this; }
 
-template < typename T, typename Container = std::deque<T> >
-it	MutantStack<T>::end() { return (this->c.end()); }
+template < typename T >
+MutantStack<T>::~MutantStack() {}
+
+template < typename T >
+typename MutantStack<T>::it	MutantStack<T>::begin() { return this->c.begin(); }
+
+template < typename T>
+typename MutantStack<T>::it	MutantStack<T>::end() { return this->c.end(); }
 
 #endif
